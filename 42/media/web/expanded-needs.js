322a9ad5359517585509605d506ed2c6f2a61c7c
Ronald Bocchichio <rbocchichio@gmail.com>
2026-09-08 15:43:08 -0400

    Added some js helper functions for normlizing values.

--- 42/media/web/expanded-needs.js
+++ 42/media/web/expanded-needs.js
@@ -1,18 +1,82 @@
-function fmt(value) {
-    // Convert to num
-    let num = Number(value);
+function isFiniteNumber(value) {
+    if (typeof value === "number") {
+        return Number.isFinite(value);
+    }
 
-    if (!Number.isFinite(num)) {
+    if (typeof value === "string") {
+        if (value.trim() === "") {
+            return false;
+        }
+
+        const num = Number(value);
+
+        return Number.isFinite(num);
+    }
+
+    return false;
+}
+
+function isFiniteNumbers(...nums) {
+    for (let num of nums) {
+        if (!isFiniteNumber(num)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function clamp(value, min, max, round, precision = 0) {
+
+    if (!isFiniteNumbers(value, min, max)) {
         return null;
     }
-    
-    // Clamp to 0-100 and round to nearest integer
-    num = Math.max(0, Math.min(100, num));
-    return `${num.toFixed(0)}%`;
+
+    // Convert to nums
+    let _value = Number(value);
+    let _min = Number(min);
+    let _max = Number(max);
+
+    if (_max < _min) {
+        return null;
+    }
+
+    let clamped = Math.max(_min, Math.min(_max, _value));
+    let rounded = round ? Number(clamped.toFixed(precision)) : clamped;
+
+    return rounded;
+}
+
+function normalizePercent(stat) {
+    if (stat === null || stat === undefined) {
+        return null;
+    }
+
+    // TODO: Need a way to send useful error messages to the console for debugging
+    if (!isFiniteNumbers(stat.value, stat.min, stat.max)) {
+        return null;
+    }
+
+    let value = Number(stat.value);
+    let min = Number(stat.min);
+    let max = Number(stat.max);
+
+    if (max <= min) {
+        return null;
+    }
+
+    value = clamp(value, min, max, false);
+
+    if (value === null) {
+        return null;
+    }
+
+    let percent = (value - min) / (max - min) * 100;
+
+    return percent;
 }
 
 function needsExpandedBarClass(value) {
-    
+
 }
 
 function needsExpandedRow(label, value, iconKey) {
